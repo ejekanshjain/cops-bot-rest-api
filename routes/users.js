303d4355dfaa5b23cb24957c05e46064111eed73
ejekanshjain <ejekanshjain@gmail.com>
2020-03-21 15:25:53 +0530

    add condition in users route to view all police users

--- routes/users.js
+++ routes/users.js
@@ -2,27 +2,51 @@ const express = require('express')
 
 const router = express.Router()
 
-const { firebaseDB1 } = require('../db')
+const { firebaseDB1, firebaseDB2 } = require('../db')
 
 const usersRef = firebaseDB1.child('Registered_Users')
+const policeRef = firebaseDB2.child('police')
 
 router.get('/', async (req, res) => {
-    const snap = await usersRef.once('value')
-    const originalUsers = snap.val()
-    const users = []
-    Object.keys(originalUsers).forEach((key, index) => {
-        let tempUser = originalUsers[key]
-        let user = {
-            _id: key,
-            IMEI: tempUser.IMEI,
-            latitude: tempUser.latitude,
-            longitude: tempUser.longitude,
-            phoneNumber: tempUser.phoneNumber
-        }
-        users.push(user)
-    })
-    const count = users.length
-    res.json({ status: 200, message: 'List of registered users', users, originalUsers, count })
+    if (req.query.police == 'true') {
+        const users = []
+        const snap = await policeRef.once('value')
+        const originalData = snap.val()
+        const regions = Object.keys(originalData).map(key => key)
+        regions.forEach(region => {
+            const regionalUsers = originalData[region]
+            Object.keys(regionalUsers).forEach(userId => {
+                const { name, number, latitude, longitude } = regionalUsers[userId]
+                users.push({
+                    _id: userId,
+                    name,
+                    phoneNumber: number,
+                    region,
+                    latitude,
+                    longitude
+                })
+            })
+        })
+        const count = users.length
+        res.json({ status: 200, message: 'List of registered users', users, count })
+    } else {
+        const snap = await usersRef.once('value')
+        const originalUsers = snap.val()
+        const users = []
+        Object.keys(originalUsers).forEach((key, index) => {
+            let tempUser = originalUsers[key]
+            let user = {
+                _id: key,
+                IMEI: tempUser.IMEI,
+                latitude: tempUser.latitude,
+                longitude: tempUser.longitude,
+                phoneNumber: tempUser.phoneNumber
+            }
+            users.push(user)
+        })
+        const count = users.length
+        res.json({ status: 200, message: 'List of registered users', users, count })
+    }
 })
 
 module.exports = router
