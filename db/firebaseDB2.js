dd8c3fd91a93e42dad4b38da9c26add5b0b5772d
ejekanshjain <ejekanshjain@gmail.com>
2020-03-21 14:54:14 +0530

    fix bug regarding police app firebase db

--- db/firebaseDB2.js
+++ db/firebaseDB2.js
@@ -4,6 +4,6 @@ const firebase = require('firebase')
 firebase.initializeApp({
     databaseURL: process.env.FIREBASE_DB2_URL,
     credential: process.env.FIREBASE_DB2_CREDENTIAL
-})
+}, 'PoliceApp')
 
-module.exports = firebase.app().database().ref()
+module.exports = firebase.app('PoliceApp').database().ref()
