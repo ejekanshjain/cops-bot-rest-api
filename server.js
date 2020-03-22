5fc99864aaf57c19ff4843d2b916534b125743e2
ejekanshjain <ejekanshjain@gmail.com>
2020-03-22 22:48:59 +0530

    add logger to log all incoming requests

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ if (process.env.NODE_ENV != 'production') {
 }
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 3000
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
 app.use(cors({
     "origin": "*",
@@ -14,6 +18,7 @@ app.use(cors({
     "optionsSuccessStatus": 204
 }))
 app.use(express.json())
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.get('/', (req, res) => {
     res.json({ status: 200, message: 'COPS Bot Server Up and Running...' })
