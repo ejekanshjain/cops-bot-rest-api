f2855708b469010cc981194990a09f93ba981d51
ejekanshjain <ejekanshjain@gmail.com>
2020-03-22 22:58:28 +0530

    lower case regions to upper case

--- utilities/regions.js
+++ utilities/regions.js
@@ -1,71 +1,71 @@
 const regions = [
     {
-        station: 'surajpole',
+        station: 'SURAJPOLE',
         latitude: 24.579461,
         longitude: 73.692903
     },
     {
-        station: 'ghantaghar',
+        station: 'GHANTAGHAR',
         latitude: 24.581966,
         longitude: 73.685488
     },
     {
-        station: 'ambamata',
+        station: 'AMBAMATA',
         latitude: 24.586098,
         longitude: 73.675075
     },
     {
-        station: 'mahila thana',
+        station: 'MAHILA THANA',
         latitude: 24.586354,
         longitude: 73.675125
     },
     {
-        station: 'goverdhan vilas',
+        station: 'GOVERDHAN VILAS',
         latitude: 24.529648,
         longitude: 73.680166
     },
     {
-        station: 'dabok',
+        station: 'DABOK',
         latitude: 24.625416,
         longitude: 73.866557
     },
     {
-        station: 'ayad',
+        station: 'AYAD',
         latitude: 24.58721,
         longitude: 73.71226
     },
     {
-        station: 'jawar mines',
+        station: 'JAWAR MINES',
         latitude: 24.35507,
         longitude: 73.733567
     },
     {
-        station: 'hathipole',
+        station: 'HATHIPOLE',
         latitude: 24.5867554,
         longitude: 73.6863064
     },
     {
-        station: 'dhanmandi',
+        station: 'DHANMANDI',
         latitude: 24.5825574,
         longitude: 73.6917501
     },
     {
-        station: 'hiran magri',
+        station: 'HIRAN MAGRI',
         latitude: 24.560167,
         longitude: 73.717357
     },
     {
-        station: 'sukher',
+        station: 'SUKHER',
         latitude: 24.616268,
         longitude: 73.72378
     },
     {
-        station: 'pratap nagar',
+        station: 'PRATAP NAGAR',
         latitude: 24.589303,
         longitude: 73.74631
     },
     {
-        station: 'bhupal pura',
+        station: 'BHUPAL PURA',
         latitude: 24.587802,
         longitude: 73.701411
     }
